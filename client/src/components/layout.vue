f573e700655078a2a1ab4538cadf7409fa243e3d
chelizichen <1347290221@qq.com>
2024-03-25 00:39:31 +0800

    FEAT:文字

--- client/src/components/layout.vue
+++ client/src/components/layout.vue
@@ -92,7 +92,7 @@ export default {
           </el-divider>
           <el-divider content-position="center">
             <div style="color: rgb(207, 15, 124); font-size: 14px">
-              SefficiencY Started From SimpCloud
+              SefficiencY Deployed From SimpCloud
             </div>
           </el-divider>
         </el-footer>
